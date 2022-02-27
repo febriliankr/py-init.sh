fa2a8ba0945d99c7a82e15ecb130754b9e283532
Febrilian Kristiawan <febriliankr@gmail.com>
2022-02-27 08:41:04 +0700

    initial commit

---
+++ py3-init.sh
@@ -0,0 +1,14 @@
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+reset=`tput sgr0`
+
+echo "Enter project name:" 
+read projectName
+echo "Running python3 -m venv" $projectName
+python3 -m venv $projectName
+echo "To continue, cd and then activate virtualenv by using: "
+echo "$ ${yellow}cd ${projectName}${reset}"
+echo "$ ${yellow}source /bin/activate${reset}"
+echo "To deactivate virtualenv by using: "
+echo "$ ${red}deactivate${reset}"
